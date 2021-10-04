740b0c5d717c936a9869c468e103ad4220980e75
JoshuaJWatt <48531152+JoshuaJWatt@users.noreply.github.com>
2021-10-04 15:34:41 +0100

    Update worlddisplay.py

--- worlddisplay.py
+++ worlddisplay.py
@@ -10,30 +10,6 @@ world = [[0,0,0,0,0],
 
 viewsize = 1
 
-# def surrounds(self, x, y):
-#         """Checks the surroundings of a cell and returns the number of lives cells around it"""
-#         arr = []
-#         for i in range (-1, 2):
-#             for j in range (-1, 2):
-#                 try:
-#                     arr.append(self.world[x+i,y+j])
-#                 except IndexError:
-#                     arr.append(0)
-#         sum_ = sum(arr) - self.world[x,y]
-#         return(sum_)
-		
-# def viewwindow(centre = (0,0)):
-# 	view = []
-# 	for i in range(viewsize):
-# 		view.append([])
-# 	for i in range(viewsize): # The row we're on
-# 		for j in range(viewsize): # the column we're on
-# 			# If we're too close to the edge, we'll get an index error, so we want to catch that
-# 			try: view[i].append(world[centre[0]-(viewsize - i)])
-# 			except IndexError:
-# 				view[i].append(' ')
-# 	return(view)
-
 def viewwindow(centre = (0,0)):
 	# This will do the columns
 	lowerbound = (centre[1]-viewsize)
