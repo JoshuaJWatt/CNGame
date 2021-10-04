ba0b58c92197bf5e361e1b2da39a88b09533d712
JoshuaJWatt <48531152+JoshuaJWatt@users.noreply.github.com>
2021-10-04 17:52:40 +0100

    Update worlddisplay.py
    we can now move the player around

--- worlddisplay.py
+++ worlddisplay.py
@@ -15,6 +15,8 @@ viewsize = 2
 
 playersprite = "x"
 
+floortile = "."
+
 def viewwindow(centre = (0,0)):
 	# This will do the columns
 	lowerbound = (centre[1]-viewsize)
@@ -55,17 +57,22 @@ def moveplayer(x = 0, y = 0):
 		try:
 			loc = [world[i].index(playersprite)]
 			loc.append(i)
-			# i[loc] = "."
+			# loc.reverse()
 			print(world[(loc[1])+y][(loc[0])+x])
 		except ValueError:
 			continue
-	if world[(loc[1])+y][(loc[0])+x] != ("." or "x"):
+		except IndexError:
+			print("Can't move that far, you'd fall off the world")
+			return(False)
+	if world[(loc[1])+y][(loc[0])+x] != floortile and world[(loc[1])+y][(loc[0])+x] != playersprite:
 		print("You can't walk through walls, sadly")
 	else:
+		world[loc[1]][loc[0]] = floortile
 		loc[0] += x
 		loc[1] += y
-	world[loc[0]][loc[1]] = "x"
+		world[loc[1]][loc[0]] = playersprite
 
 # printview(viewwindow((4,4)))
 
-moveplayer()
+moveplayer(0, -3)
+printview(world)
